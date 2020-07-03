4bccfd03cd4264ab3f8324afbb58c2f08909de09
Motoki Kawakami <mothule.dev+github@gmail.com>
2020-07-03 15:57:06 +0900

    Add project

---
+++ globee_kadai/globee_kadai/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  globee_kadai
+//
+//  Created by motoki kawakami on 2020/07/03.
+//  Copyright © 2020 mothule. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
