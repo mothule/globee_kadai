5a50029a429271b22374d740b19e4d12d1efebe5
Motoki Kawakami <mothule.dev+github@gmail.com>
2020-07-15 13:29:02 +0900

    Prepare fonts theme

--- globee_kadai/Presentations/Theme/Theme.swift
+++ globee_kadai/Presentations/Theme/Theme.swift
@@ -15,6 +15,13 @@ struct Theme {
         var primary: UIColor = UIColor(named: "primary")!
         var secondary: UIColor = UIColor(named: "secondary")!
     }
+    struct Fonts {
+        var jaRegularName = "HiraKakuProN-W3"   // 日本語 通常フォント名
+        var jaBoldName = "HiraKakuProN-W6"      // 日本語 太字フォント名
+        var regularName = "AvenirNext-Regular"  // 英字/数字/記号 通常フォント名
+        var boldName = "AvenirNext-Bold"        // 英字/数字/記号 太字フォント名
+    }
     
     static var color: Colors = Colors()
+    // static var fonts: Fonts = Fonts() フォントが必要になれば
 }
